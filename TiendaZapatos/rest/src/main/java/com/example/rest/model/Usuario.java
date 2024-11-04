c1e21ab6dbdd958311874425568366eaaca04a97
LeonardoEspinoza7373 <166523336+LeonardoEspinoza7373@users.noreply.github.com>
2024-11-03 21:32:20 -0500

    Delete TiendaZapatos directory

--- TiendaZapatos/rest/src/main/java/com/example/rest/model/Usuario.java
+++
@@ -1,9 +0,0 @@
-package com.example.rest.model;
-
-public class Usuario {
-    private String user;
-    private String password;
-    private String email;
-    private boolean status;
-
-}
--- TiendaZapatos/rest/src/main/java/com/example/rest/services/UsuarioService.java
+++
@@ -1,5 +0,0 @@
-package com.example.rest.services;
-
-public class UsuarioService {
-    
-}
