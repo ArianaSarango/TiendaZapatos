f2374bdcdc48039e8a7e9d3c2004de5fb6a49693
Ariana Sarango <ariana.s.sarango@unl.edu.ec>
2024-11-25 08:45:18 -0500

    Update Template

---
+++ jersey-service/src/main/java/com/example/rest/Response.java
@@ -0,0 +1,12 @@
+package com.example.rest;
+
+import java.util.HashMap;
+
+public class Response {
+
+    public static Object ok(HashMap<String mapa) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ok'");
+    }
+
+}
